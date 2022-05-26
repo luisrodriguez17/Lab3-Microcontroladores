fef6d67577e5c6e8da2a32deaf56d0f3ac8029ca
Luis Rodriguez <redpro17@gmail.com>
2022-05-25 23:48:18 -0600

    Monitor y puerto serial en proceso

--- incubadora.ino/incubadora.ino.ino
+++ incubadora.ino/incubadora.ino.ino
@@ -1,5 +1,6 @@
 #include <math.h>
-
+#include <Adafruit_PCD8544.h>
+#include <util/delay.h>
 const int Rc = 10000; //valor de la resistencia
 const int Vcc = 5;
 const int temp_sensor = A0;
@@ -9,23 +10,47 @@ const int selector = A2;
 float A = 0.0006973550913078027;
 float B = 0.00028800736970464863;
 float C = 5.400097451986799e-9;
+float temp_set = 0;
+float hum = 0;
+float celsius = 0;
 
 float K = 2.5; //factor de disipacion en mW/C
+
+Adafruit_PCD8544 display = Adafruit_PCD8544(7,5, 6, 4, 8);
+
+void display_screen(){
+  display.setCursor(0,0);
+  display.print("T.set: ");
+  display.println(temp_set);
+  display.print("Temp: ");
+  display.println(celsius);
+  display.print("Humid %: ");
+  display.println(hum);
+  display.display();
+  display.clearDisplay();
+  }
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
+  display.begin();
+  display.setContrast(75);
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
 }
 
 void loop() 
 {
   float raw_temp = analogRead(temp_sensor);
   float raw_hum = analogRead(hum_sensor);
-  float hum = map(raw_hum, 0, 1023, 0, 2000);
+  hum = map(raw_hum, 0, 1023, 0, 200);
   float raw_temp_set = analogRead(selector);
-  float temp_set = map(raw_temp_set, 0, 1023, 0, 120);
+  temp_set = map(raw_temp_set, 0, 1023, 0, 120);
   // Config PID meta value
   if(temp_set < 30){
     temp_set = 30;
@@ -48,7 +73,7 @@ void loop()
   float R_th = 1.0 / (A + B * logR + C * logR * logR * logR );
 
   float kelvin = R_th - V*V/(K * R)*1000;
-  float celsius = kelvin - 273.15;
+  celsius = kelvin - 273.15;
 
   if(celsius >= 42){
     digitalWrite(13, HIGH);
@@ -67,5 +92,5 @@ void loop()
   Serial.print("T = ");
   Serial.print(celsius);
   Serial.print("C\n");
-  delay(2500);
+  display_screen();
 }
